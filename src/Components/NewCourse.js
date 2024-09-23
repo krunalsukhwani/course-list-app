4adb26aab5c852c7cc620c9f317c0e55430321f5
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-23 18:14:35 -0400

    Two way binding - state management

--- src/Components/NewCourse.js
+++ src/Components/NewCourse.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import "./NewCourse.css";
 
 const NewCourse = props => {
+
+  const [enteredCourse, setEnteredCourse] = useState('');
 
   const addCourseHandler = (event) => {
     event.preventDefault();
     const newCourse = {
       id: Math.random().toString(),
-      text: "JAVA EE Programming",
+      text: enteredCourse,
     };
+    
+    setEnteredCourse('');
+
     props.onAddCourse(newCourse);
+  };
+
+  const textChangeHandler = event => {
+    setEnteredCourse(event.target.value);
   };
 
   return (
     <form className="new-course" onSubmit={addCourseHandler}>
-      <input type="text" />
+      <input type="text" value={enteredCourse} onChange={textChangeHandler}/>
       <button type="submit">Add Course</button>
     </form>
   );
